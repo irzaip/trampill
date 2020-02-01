5e426eccf95589450ffe681d8f28aea84de749ce
Irza Pulungan <irzaip@gmail.com>
2020-02-01 18:30:40 +0700

    Add Video

--- lib/screen/pg15_materi.dart
+++ lib/screen/pg15_materi.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/physics.dart';
 import 'package:quiver/strings.dart';
-
+import 'package:flutter_youtube/flutter_youtube.dart';
+import 'package:trampill/services/keys.dart';
 
 class Pg15_Materi extends StatefulWidget {
   @override
@@ -12,20 +13,36 @@ class _Pg15_MateriState extends State<Pg15_Materi> {
   Map sampleContent = {
     '1': [
       "Content 1 book",
-      "Content 2",
-      "(https://youtube.com/irzaip/)",
+      "Content 2\n\nKonten ini sangat panjang semoga anda terhibur dengan tulisan ini, dan kamipun menjadi senang.",
+      "(https://www.youtube.com/watch?v=tc_8yy39RxE)",
+      "(https://www.youtube.com/watch?v=bg6vYLs5DKg)",
+      "(https://www.youtube.com/watch?v=ndFiHFGW8oQ)",
       "Content 4"
     ],
     '2': ["Content 2-1", "Content 2-2", "Content 2-3"],
     '3': ["Content 3-1", "Content 3-2"]
   };
 
+  void playYoutubeVideo(youtubeURL) {
+    FlutterYoutube.playYoutubeVideoByUrl(
+      apiKey: API_KEY,
+      videoUrl: youtubeURL,
+    );
+  }
+
   buildContent(Map content, int index) {
     List<Widget> content = new List<Widget>();
     String cntdx = sampleContent.keys.toList()[index].toString();
 
-    for (var i = 0; i < sampleContent.length; i++) {
-      if (sampleContent[cntdx][i].toString().contains("youtube")) {
+    for (var i = 0; i < sampleContent[cntdx].length; i++) {
+      String isi = sampleContent[cntdx][i].toString();
+      if (isi.contains("youtube")) {
+        isi = isi.replaceAll("(", "").replaceAll(")", "");
+        Uri yturl = Uri.parse(isi);
+        String videoID = yturl.queryParameters['v'].toString();
+        String ytimgURL = "http://img.youtube.com/vi/" + videoID +"/0.jpg";
+
+
         content.add(
           Padding(
             padding: const EdgeInsets.all(20.0),
@@ -34,18 +51,24 @@ class _Pg15_MateriState extends State<Pg15_Materi> {
               child: Center(
                 child: Column(
                   children: <Widget>[
+                    SizedBox(height: 20,),
+                    Padding(
+                      padding: const EdgeInsets.all(15.0),
+                      child: Image.network(ytimgURL),
+                    ),
                     Text(
                       "YOUTUBE VIDEO",
                       style: TextStyle(
-                        fontSize: 30,
+                        fontSize: 20,
                         color: Colors.black,
                       ),
                     ),
+                    SizedBox(height: 20,),
                     RaisedButton(
                       onPressed: () {
-                        callBottomsheet();
+                        playYoutubeVideo(isi);
                       },
-                      child: Text("Menu"),
+                      child: Text("Buka"),
                     )
                   ],
                 ),
@@ -60,11 +83,14 @@ class _Pg15_MateriState extends State<Pg15_Materi> {
             child: new Container(
               color: Colors.white,
               child: Center(
-                child: Text(
-                  sampleContent[cntdx][i],
-                  style: TextStyle(
-                    fontSize: 22,
-                    color: Colors.black,
+                child: Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: Text(
+                    sampleContent[cntdx][i],
+                    style: TextStyle(
+                      fontSize: 26,
+                      color: Colors.black,
+                    ),
                   ),
                 ),
               ),
