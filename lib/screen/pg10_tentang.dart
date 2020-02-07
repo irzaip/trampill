646dc488a3b139a76858f257dd317f61c4aea9ca
Irza Pulungan <irzaip@gmail.com>
2020-02-07 21:39:40 +0700

    Add markdown

--- lib/screen/pg10_tentang.dart
+++ lib/screen/pg10_tentang.dart
@@ -1,30 +1,122 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-
+import 'package:flutter_markdown/flutter_markdown.dart';
 
 class Pg10Tentang extends StatelessWidget {
+  final String _markdownData = """
+# Markdown Example
+Markdown allows you to easily include formatted text, images, and even formatted Dart code in your app.
+
+## Titles
+
+Setext-style
+
+```
+This is an H1
+=============
+
+This is an H2
+-------------
+```
+
+Atx-style
+
+```
+# This is an H1
+
+## This is an H2
+
+###### This is an H6
+```
+
+Select the valid headers:
+
+- [x] `# hello`
+- [ ] `#hello`
+
+## Links
+
+[Google's Homepage][Google]
+
+```
+[inline-style](https://www.google.com)
+
+[reference-style][Google]
+```
+
+## Images
+
+![Flutter logo](/dart-lang/site-shared/master/src/_assets/image/flutter/icon/64.png)
+
+## Tables
+
+|Syntax                                 |Result                               |
+|---------------------------------------|-------------------------------------|
+|`*italic 1*`                           |*italic 1*                           |
+|`_italic 2_`                           | _italic 2_                          |
+|`**bold 1**`                           |**bold 1**                           |
+|`__bold 2__`                           |__bold 2__                           |
+|`This is a ~~strikethrough~~`          |This is a ~~strikethrough~~          |
+|`***italic bold 1***`                  |***italic bold 1***                  |
+|`___italic bold 2___`                  |___italic bold 2___                  |
+|`***~~italic bold strikethrough 1~~***`|***~~italic bold strikethrough 1~~***|
+|`~~***italic bold strikethrough 2***~~`|~~***italic bold strikethrough 2***~~|
+
+## Styling
+Style text as _italic_, __bold__, ~~strikethrough~~, or `inline code`.
+
+- Use bulleted lists
+- To better clarify
+- Your points
+
+## Code blocks
+Formatted Dart code looks really pretty too:
+
+```
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      body: Markdown(data: markdownData),
+    ),
+  ));
+}
+```
+
+## Markdown widget
+
+This is an example of how to create your own Markdown widget:
+
+    Markdown(data: 'Hello _world_!');
+
+Enjoy!
+
+[Google]: https://www.google.com/
+""";
+
+
   @override
   Widget build(BuildContext context) {
+    final controller = ScrollController();
+
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
         backgroundColor: Colors.blue,
         title: Text('Tentang kami'),
       ),
-      body: Column(
-          children: <Widget>[
-            Text("Hello"),
-            RichText(
-              text: TextSpan(
-                text: 'Hello ',
-                style: DefaultTextStyle.of(context).style,
-                children: <TextSpan>[
+      body: Container(
+        color: Colors.black,
+        child: SafeArea(
+          child: Markdown(
+            controller: controller,
+            selectable: false,
+            data: _markdownData,
+            imageDirectory: 'https://raw.githubusercontent.com',
+          ),
+        ),
 
-                  TextSpan(text: ' INDOSOAI!'),
-                ],
-              ),
-            )
-          ],
+
+
       ),
     );
   }
